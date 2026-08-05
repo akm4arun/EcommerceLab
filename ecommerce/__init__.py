c2b44c2dde79c38e78bb7a77a1fcec35fb286905
Arun Mishra <arunkumarmishra058@gmail.com>
2026-08-06 00:21:01 +0530

    feat: initialize SQLAlchemy extension

--- ecommerce/__init__.py
+++ ecommerce/__init__.py
@@ -1,6 +1,27 @@
-from flask import Flask, app
+# from flask import Flask, app
+
+# from config import Config
+# from ecommerce.routes.home import home_bp
+
+
+# def create_app():
+
+#     app = Flask(__name__)
+
+#     app.config.from_object(Config)
+
+#     print("APP_NAME =", app.config.get("APP_NAME"))
+#     print("SECRET_KEY =", app.config.get("SECRET_KEY"))
+
+#     app.register_blueprint(home_bp)
+
+#     return app
+
+from flask import Flask
 
 from config import Config
+
+from ecommerce.extensions import db
 from ecommerce.routes.home import home_bp
 
 
@@ -10,8 +31,7 @@ def create_app():
 
     app.config.from_object(Config)
 
-    print("APP_NAME =", app.config.get("APP_NAME"))
-    print("SECRET_KEY =", app.config.get("SECRET_KEY"))
+    db.init_app(app)
 
     app.register_blueprint(home_bp)
 
