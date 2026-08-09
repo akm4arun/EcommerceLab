e40fd29d4890b27411b3b3873bbafc8b6687ae35
Arun Mishra <arunkumarmishra058@gmail.com>
2026-08-09 15:18:10 +0530

    Create instance folder in container startup

--- app.py
+++ app.py
@@ -6,10 +6,13 @@
 
 from ecommerce import create_app
 from config import get_config
+import os
 
 app = create_app(get_config())
 
-# Register app config for templates BEFORE app.run()
+# Ensure instance folder exists in Azure Container App
+os.makedirs(app.instance_path, exist_ok=True)
+
 @app.context_processor
 def inject_config():
     return dict(app_config=app.config)
